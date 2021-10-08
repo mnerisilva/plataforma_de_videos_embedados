d3664adbc1ea1f15d4bf9e295e6198f66fec8b33
mnerisilva <marceloneri@gmail.com>
2021-10-08 16:23:31 -0300

    update

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let _src = '';
 
 let _tag = '';
 
+let todas_li = '';
+
 midia.forEach(function(_midia){
     let elementUL = document.createElement('ul');      
     let elementLI = document.createElement('li');
@@ -31,7 +33,7 @@ midia.forEach(function(_midia){
     elementLI.setAttribute('data-id',_midia.id);
     elementLI.innerHTML = _midia.title;
     elementLI.addEventListener('click', function(){
-        let todas_li = document.querySelectorAll('.left-side li');
+        todas_li = document.querySelectorAll('.left-side li');
         todas_li.forEach(li => li.classList.remove('active'));
         const _this = this;
         _this.classList.add('active');
@@ -39,9 +41,11 @@ midia.forEach(function(_midia){
             _src = `https://www.youtube.com/embed/${ _midia.videos_playlist[0] }?playlist=${ _midia.videos_playlist.toString()}&enablejsapi=1&amp;version=3&amp;playerapiid=ytplayer&amp;showinfo=0&amp;modestbranding=1&amp;fs=0&amp;cc_load_policy=1;showinfo=0;autohide=1;rel=0`;
         right_side_h4.textContent = _midia.title;
         iframe.setAttribute('src', _src)
-    });
-    
+    });    
 });
+
+todas_li = document.querySelectorAll('.left-side li');
+todas_li[0].classList.add('active');
 
 _src = `https://www.youtube.com/embed/${ midia[0].videos_playlist[0] }?playlist=${ midia[0].videos_playlist.toString() }&enablejsapi=1&amp;version=3&amp;playerapiid=ytplayer&amp;showinfo=0&amp;modestbranding=1&amp;fs=0&amp;cc_load_policy=1;showinfo=0;autohide=1;rel=0`;
 
